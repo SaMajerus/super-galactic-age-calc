bc148c0e7c5d6e1ed5cb3300f86282a4c40b03f0
Samuel Majerus <ladolego@gmail.com>
2022-09-02 11:17:04 -0700

    First test of ageCalc passed

--- src/js/ageCalc.js
+++ src/js/ageCalc.js
@@ -1,3 +1,5 @@
 export function daysSince(today, born) {
-  return(today-born); 
+  let msSince = today-born; 
+  let msToDays = msSince / 86400000;  //1 Day = 86,400,000 milliseconds (ms)
+  return(msToDays); 
 }
