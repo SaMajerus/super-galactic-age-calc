4541254b075c7def1b903628c281ae1251c37cc3
Samuel Majerus <ladolego@gmail.com>
2022-09-02 10:40:29 -0700

    First test -- bad(questionmark) fail

---
+++ __tests__/ageCalc.test.js
@@ -0,0 +1,9 @@
+import { ageCalc } from '../src/js/ageCalc.js';
+
+describe('ageinDays', () => {
+  
+  test("Should return user's correct age in days, based on entered birthdate", () => {
+    
+    expect(bdDays).toEqual();
+  });
+});
