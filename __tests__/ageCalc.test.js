6fb8dad2171a4af5e64e03c2d9e7a7b7672db9d2
Samuel Majerus <ladolego@gmail.com>
2022-09-02 12:04:24 -0700

    'inMercuryYrs' function test 1 -- good fail.

--- __tests__/ageCalc.test.js
+++ __tests__/ageCalc.test.js
@@ -1,13 +1,23 @@
 import { daysSince } from '../src/js/ageCalc.js';
+import { inMercuryYrs } from '../src/js/ageCalc.js';
 
-describe('userAgeInDays', () => {
+describe('daysSince', () => {
   
   test("Should return user's correct age in days, based on entered birthdate", () => {
-    //When calling Date in the fashion shown below  ('year number', 'month number', 'date number'), note this:   the Month number goes from 0-11.
+    //When calling Date like below  ('year number', 'month number', 'date number'), note this:   the Month number goes from 0-11.
     let bday = new Date(2000, 4, 14); //User's entered birthday [using my own in this test]. 
     let d8Today = new Date(2022, 8, 2);  //Today's date, entered by user.
     let ageDays = daysSince(d8Today, bday); //Age in days 
 
     expect(ageDays).toEqual(8146);  //As of today (9-2-2022), this is how many days since 5-14-2000.
+  });
+});
+describe('inMercuryYrs', () => { 
+
+  test("Should return user's correct age in years, based on their age in Days", () => {
+    const earthAge = 8146; //My age (in Earth days)  
+    let age = inMercuryYrs(earthAge);
+    
+    expect(age).toEqual(92.99); 
   });
 });
--- src/js/ageCalc.js
+++ src/js/ageCalc.js
@@ -1,5 +1,23 @@
+/*     Business Logic     */
+
 export function daysSince(today, born) {
-  let msSince = today-born; 
-  let msToDays = msSince / 86400000;  //1 Day = 86,400,000 milliseconds (ms)
-  return(msToDays); 
+  return((today-born)/86400000);  //1 Day = 86,400,000 milliseconds (ms)
+} 
+
+export function inMercuryYrs(ageDays){ //Takes a parameter containing the user's age in Earth-days, and returns their age in Mercury-years.  
+  //1 solar year on Mercury = 87.6 Earth-Days. 
 }
+
+/*
+export function inVenusYrs(ageDays){ //Takes a parameter containing the user's age in Earth-days, and returns their age in Venus-years. 
+  //1 Solar Year on Venus = 226.3 Earth-Days. 
+}
+
+export function inMarsYrs(ageDays){ //Takes a parameter containing the user's age in Earth-days, and returns their age in Mars-years. 
+  //1 Solar Year on Mars = 686.2 Earth-Days. 
+}
+
+export function inJuptierYrs(ageDays){ //Takes a parameter containing the user's age in Earth-days, and returns their age in Jupiter-years. 
+  //1 Solar Year on Venus = 4328.9 Earth-Days. 
+}
+*/
